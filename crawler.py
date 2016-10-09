ce04feb4fceb4bcee7fbb0bc7cf06ebdfd7c047c
XuYan <abaloon2015@gmail.com>
2016-10-09 10:10:47 -0700

    Get ready to fill crawling logic into code skeleton

--- crawler.py
+++ crawler.py
@@ -1,7 +1,25 @@
 import argparse
 
-parser = argparse.ArgumentParser(description='Web crawler')
-parser.add_argument('-url', nargs='?', required=True, help='the url of a webpage to start with')
-parser.add_argument('-css', nargs='+', required=True, help='the list of css to capture webpage information')
+class crawler():
+	def __init__(self, url, css_selectors):
+		self.url = url
+		self.css_selectors = css_selectors
 
-parser.parse_args('-url www.google.com -css #id1 .class1'.split());
+	def crawl(self):
+		print("Crawling...")
+
+
+def parseArgs():
+	""" Parse command line arguments """
+	# TODO: Add usage and more robust error checking logic
+	parser = argparse.ArgumentParser(description='Web crawler')
+	parser.add_argument('-url', nargs='?', required=True, help='the url of a webpage to start with')
+	parser.add_argument('-css', nargs='+', required=True, help='the list of css to capture webpage information')
+	args = parser.parse_args();
+
+	return (args.url, args.css)
+
+if __name__ == '__main__':
+	url, css_selectors = parseArgs()
+	crawler = crawler(url, css_selectors)
+	crawler.crawl()
